b42c96618114e7db5d344ac3080444a017c17b1c
Jean Carlos <5054292+jean-cap@users.noreply.github.com>
2024-02-02 17:28:32 -0300

    Módulo 1 - Aula 8
    A aula falou sobre operador de incremento e decremento, mostrando a diferença entre o pré e o pós.

---
+++ modulo_01/operadores_de_incremento_e_decremento/operadores_de_incremento_e_decremento_1.php
@@ -0,0 +1,17 @@
+<?php
+
+// Incremento e decremento:
+
+$number = 10;
+
+// Primeiro incrementa e depois retorna o valor (pré-incremento):
+// echo ++$number; // 11
+
+// Primeiro retorna o valor e depois incrementa (pós-incremento):
+// echo $number++; // 10
+
+// Primeiro subtrai e depois retorna o valor (pré-decremento):
+// echo --$number; // 9
+
+// Primeiro retorna o valor e depois subtrai (pós-decremento):
+echo $number--; // 10
