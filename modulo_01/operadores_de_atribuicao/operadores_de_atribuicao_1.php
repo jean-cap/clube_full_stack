f3fa24be07e4b60ae611217148c5611b2e66e614
Jean Carlos <5054292+jean-cap@users.noreply.github.com>
2024-02-02 17:20:08 -0300

    Módulo 1 - Aula 7
    A aula falou sobre o operador de atribuição e também sobre a junção de operador aritmético e atribuição para simplificar uma expressão.

---
+++ modulo_01/operadores_de_atribuicao/operadores_de_atribuicao_1.php
@@ -0,0 +1,12 @@
+<?php
+
+// O sinal de igual (=) indica que estou atribuindo o valor "Jean Carlos" à variável $name:
+$name = 'Jean';
+
+// Para unir duas strings, usamos o ponto (.):
+// $name = $name . ' Carlos';
+
+// Uma simplificação da expressão acima, para unir duas strings é ponto e igual (.=):
+$name .= ' Carlos';
+
+echo $name; // Jean Carlos
