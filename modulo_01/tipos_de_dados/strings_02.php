9805621d1d7708f24dca40affc61680e35d3d2b5
Jean Carlos <5054292+jean-cap@users.noreply.github.com>
2024-02-02 22:12:41 -0300

    Módulo 1 - Aula 15
    A aula falou sobre strings.

---
+++ modulo_01/tipos_de_dados/strings_02.php
@@ -0,0 +1,9 @@
+<?php
+
+$age = 18;
+$name = 'Jean Carlos';
+
+// Quando usamos aspas duplas, o PHP interpreta as variáveis dentro da string:
+$text = "Meu nome é $name e tenho $age anos.";
+
+echo $text; // Meu nome é Jean Carlos e tenho 18 anos.
